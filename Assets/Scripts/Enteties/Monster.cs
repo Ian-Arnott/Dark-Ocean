89e1e0c8ead8d4e3e13d047684c4dfaaac686f0a
Ian <iarnott@itba.edu.ar>
2023-06-28 22:51:19 -0300

    Improvement on Monster, added particle system

--- Assets/Scripts/Enteties/Monster.cs
+++ Assets/Scripts/Enteties/Monster.cs
@@ -8,72 +8,101 @@ public class Monster : MonoBehaviour
     [SerializeField] private List<Transform> _locations;
     [SerializeField] private float _timeMax = 5f; // Maximum time to trigger the game end
     [SerializeField] private int _teleportChance = 20; // Chance of teleporting instead of following player
-
     [SerializeField] private NavMeshAgent _agent; // Reference to the NavMeshAgent component
-    private float _currentTime = 0f; // Current time the player has been looking at the monster
-    private bool _isFollowing = false; // Flag to indicate if the monster is following the player
-
     [SerializeField] private GameObject _player;
     [SerializeField] private Vector3 _offset;
-    private bool _detected;
-    private bool _seen;
+    [SerializeField] private float _speed;
+    private float _currentTime = 0f; // Current time the player has been looking at the monster
+    private float _timeSinceTeleported = 0f;
+    private bool _isFollowing = false; // Flag to indicate if the monster is following the player
+    private bool _teleported = true;
+    private bool _detected = false;
+    private bool _seen = false;
+    private Vector3 _previousPosition;
 
     private void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _speed = 3f;
+        _agent.enabled = false;
+        _previousPosition = transform.position;
         TeleportToLocation(_locations[0]); // Start at the first location
+        EventManager.instance.OnKeyPickup += IncreaseDifficulty;
     }
 
     void Update()
     {
-        if (IsMonsterVisible() && DetectPlayer())
+        if(_teleported) _timeSinceTeleported += Time.deltaTime;
+
+        if (IsMonsterVisible() && DetectPlayer()) HandleVisibleMonster();
+        else HandleInvisibleMonster();
+
+    }
+
+    private void HandleVisibleMonster()
+    {
+        _currentTime += Time.deltaTime;
+        EventManager.instance.LookTimeChange(_currentTime);
+
+        if (_currentTime >= _timeMax) EventManager.instance.EventGameOver(false);
+
+        _agent.enabled = false;
+        _isFollowing = false;
+        _seen = true;
+    }
+
+    private void HandleInvisibleMonster()
+    {
+        if (_currentTime > 0)
         {
-            _currentTime += Time.deltaTime;
+            _currentTime -= Time.deltaTime;
             EventManager.instance.LookTimeChange(_currentTime);
-            // Trigger event when time reaches maximum
-            if (_currentTime >= _timeMax)
-            {
-                EventManager.instance.EventGameOver(false);
-            }
-            _agent.enabled = false;
-            _isFollowing = false;
-            _seen = true;
         }
         else
         {
-            if (_currentTime > 0)
-            {
-                _currentTime -= Time.deltaTime;
-                EventManager.instance.LookTimeChange(_currentTime);
-            }
-            else
-            {
-                _currentTime = 0f;
-                EventManager.instance.LookTimeChange(_currentTime);
-            }
-
-            int aux = Random.Range(0, 100);
-
-            if (_seen && !_isFollowing && aux < _teleportChance)
-            {
-                // Teleport to a random location
-                int randomIndex = Random.Range(0, _locations.Count);
-                TeleportToLocation(_locations[randomIndex]);
-                _seen = false;
-            }
-            else if (_seen && !_isFollowing && aux > _teleportChance)
-            {
-                _agent.enabled = true;
-                _agent.SetDestination(_player.transform.position);
-                _isFollowing = true;
-                _seen = false;
-            }
-            else if (!_seen && _isFollowing && aux > _teleportChance)
-            {
-                _agent.SetDestination(_player.transform.position);
-                _isFollowing = true;
-            }
+            _currentTime = 0f;
+            EventManager.instance.LookTimeChange(_currentTime);
         }
+
+        int d100 = 0;
+        if (_seen) d100 = Random.Range(0, 100);
+
+        if (_seen && !_isFollowing && d100 < _teleportChance) TeleportMonster();
+        else if (_seen && !_isFollowing && d100 >= _teleportChance) FollowPlayer();
+        else if (!_seen && _isFollowing && d100 >= _teleportChance) FollowPlayer();
+        
+        if (_teleported && _timeSinceTeleported >= 2 * _timeMax) TeleportMonster();
+        
+    }
+
+    private void TeleportMonster()
+    {
+        int randomIndex = Random.Range(0, _locations.Count);
+        TeleportToLocation(_locations[randomIndex]);
+        _agent.enabled = false;
+        _agent.speed = 0f;
+        _seen = false;
+        _teleported = true;
+        _timeSinceTeleported = 0f;
+    }
+
+    private void FollowPlayer()
+    {
+        _agent.enabled = true;
+        _agent.speed = _speed;
+        _agent.SetDestination(_player.transform.position);
+        _isFollowing = true;
+        _seen = false;
+        _teleported = false;
+        _timeSinceTeleported = 0f;
+    }
+
+
+    private void IncreaseDifficulty()
+    {
+        _speed += 1f;
+        _timeMax -= 0.5f;
+        _teleportChance -= 5;
     }
     private bool IsMonsterVisible()
     {
